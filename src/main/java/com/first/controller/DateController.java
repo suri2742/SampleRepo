f4b6a81bce4be204254afa7b04f322d8b2ed3b0c
Surendra <surendra.u2742@gmail.com>
2022-03-28 21:15:51 +0530

    Signed-off-by: Surendra <surendra.u2742@gmail.com>

---
+++ src/main/java/com/first/controller/DateController.java
@@ -0,0 +1,28 @@
+package com.first.controller;
+
+import java.util.Date;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class DateController {
+
+	@RequestMapping("/date")
+	public ModelAndView displayDate() {
+		
+		ModelAndView mva = new ModelAndView();
+		mva.addObject("msg","Today's date is "+new Date());
+		mva.setViewName("welcome");
+		return mva;
+	}
+
+	@RequestMapping(value={"/day","/dayname"})
+	public String diplayDate(Model model) {
+		
+		model.addAttribute("msg", "Today is "+new Date().getDay());
+		return "welcome";
+	}
+}
---
+++ src/main/java/com/first/controller/WelcomeController.java
@@ -0,0 +1,19 @@
+package com.first.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class WelcomeController {
+
+	@RequestMapping("/welcome")
+	public ModelAndView displayWelcomeMsg() {
+		
+		ModelAndView mva = new ModelAndView();
+		mva.addObject("msg", "Welcome to Spring Boot!");
+		mva.setViewName("welcome");
+		
+		return mva;
+	}
+}
